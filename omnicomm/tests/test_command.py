75eca93558f3b9881fc53aae7c139a7de660948b
Sergei Pikhovkin <pikhovkin@gmail.com>
2023-09-25 20:05:22 +0300

    Add tests for 0x93 and 0x94 commands

--- omnicomm/tests/test_command.py
+++ omnicomm/tests/test_command.py
@@ -26,3 +26,44 @@ class Cmd86Test(TestCase):
         )
         cmd86 = commands.Cmd86.unpack(msg)
         self.assertTrue(commands.Cmd86(cmd86.value).pack() == msg)
+
+
+class Cmd93Test(TestCase):
+    def setUp(self) -> None:
+        protocol.Protocol.load_command_proto()
+
+    def test_simple(self):
+        msg = bytes.fromhex('')
+        cmd93 = commands.Cmd93.unpack(msg)
+        self.assertTrue(commands.Cmd93(cmd93.value).pack() == msg)
+        value = {}
+        data = commands.Cmd93(value).pack()
+        self.assertTrue(commands.Cmd93.unpack(data).value == value)
+
+
+class Cmd94Test(TestCase):
+    def setUp(self) -> None:
+        protocol.Protocol.load_command_proto()
+
+    def test_simple(self):
+        msg = bytes.fromhex('c4a6b51b')
+        cmd94 = commands.Cmd94.unpack(msg)
+        self.assertTrue(commands.Cmd94(cmd94.value).pack() == msg)
+        value1 = {'omnicomm_time': 464889540}
+        data = commands.Cmd94(value1).pack()
+        self.assertTrue(commands.Cmd94.unpack(data).value['omnicomm_time'] == value1['omnicomm_time'])
+        value2 = {'unix_time': 1695657540}
+        data = commands.Cmd94(value2).pack()
+        self.assertTrue(commands.Cmd94.unpack(data).value['omnicomm_time'] == value1['omnicomm_time'])
+
+
+class Cmd95Test(TestCase):
+    def setUp(self) -> None:
+        protocol.Protocol.load_command_proto()
+
+    def test_simple(self):
+        msg = bytes.fromhex(
+            '67f30000ae8b12080044000a01011308ae97ca4020072800300038e807407c50cc01142b08fec785940410d69bbce6021803208802280830e0202c330800100018e20220003443083810be10180444'  # noqa: E501
+        )
+        cmd95 = commands.Cmd95.unpack(msg)
+        self.assertTrue(commands.Cmd86(cmd95.value).pack() == msg)
--- omnicomm/tests/test_protocol.py
+++ omnicomm/tests/test_protocol.py
@@ -1,3 +1,4 @@
+import time
 from unittest import TestCase
 
 from omnicomm import commands, protocol
@@ -23,7 +24,7 @@ class ProtocolTest(TestCase):
 
     def test_86(self):
         value = {
-            'num_rec': 0,
+            'rec_id': 0,
             'omnicomm_time': 0,
             'unix_time': commands.Cmd85.BASE_TIME,
             'priority': 0,
@@ -53,4 +54,66 @@ class ProtocolTest(TestCase):
         data = protocol.ServerProtocol.pack(commands.Cmd86(value))
         cmd, remain = protocol.RegistrarProtocol.unpack(data)
         self.assertTrue(cmd.id == commands.Cmd86.id)
+        self.assertTrue(cmd.value == value)
+
+    def test_93_from_server(self):
+        value = {}
+        data = protocol.ServerProtocol.pack(commands.Cmd93(value))
+        cmd, remain = protocol.RegistrarProtocol.unpack(data)
+        self.assertTrue(cmd.id == commands.Cmd93.id)
+        self.assertTrue(cmd.value == value)
+
+    def test_93_from_registrar(self):
+        value = {}
+        data = protocol.RegistrarProtocol.pack(commands.Cmd93(value))
+        cmd, remain = protocol.ServerProtocol.unpack(data)
+        self.assertTrue(cmd.id == commands.Cmd93.id)
+        self.assertTrue(cmd.value == value)
+
+    def test_94_from_server(self):
+        value = {'unix_time': int(time.time())}
+        data = protocol.ServerProtocol.pack(commands.Cmd94(value))
+        cmd, remain = protocol.RegistrarProtocol.unpack(data)
+        self.assertTrue(cmd.id == commands.Cmd94.id)
+        self.assertTrue(cmd.value['unix_time'] == value['unix_time'])
+
+    def test_94_from_registrar(self):
+        value = {'unix_time': int(time.time())}
+        data = protocol.RegistrarProtocol.pack(commands.Cmd94(value))
+        cmd, remain = protocol.ServerProtocol.unpack(data)
+        self.assertTrue(cmd.id == commands.Cmd94.id)
+        self.assertTrue(cmd.value['unix_time'] == value['unix_time'])
+
+    def test_95(self):
+        value = {
+            'rec_id': 0,
+            'omnicomm_time': 0,
+            'unix_time': commands.Cmd95.BASE_TIME,
+            'priority': 0,
+            'msgs': [
+                {
+                    'mID': [1],
+                    'general': {
+                        'Time': 135433134,
+                        'FLG': 7,
+                        'Mileage': 0,
+                        'VImp': 0,
+                        'TImp': 1000,
+                        'Uboard': 124,
+                        'SumAcc': 102,
+                    },
+                    'nav': {
+                        'LAT': 557887999,
+                        'LON': 375883499,
+                        'GPSVel': 3,
+                        'GPSDir': 264,
+                        'GPSNSat': 8,
+                        'GPSAlt': 2096,
+                    },
+                },
+            ],
+        }
+        data = protocol.ServerProtocol.pack(commands.Cmd95(value))
+        cmd, remain = protocol.RegistrarProtocol.unpack(data)
+        self.assertTrue(cmd.id == commands.Cmd95.id)
         self.assertTrue(cmd.value == value)
