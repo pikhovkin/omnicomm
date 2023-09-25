2e02ddd07d85e5e36e57e5145a2a6600fe886858
Sergei Pikhovkin <pikhovkin@gmail.com>
2023-09-25 20:40:49 +0300

    Fix settings

--- omnicomm/settings.py
+++ omnicomm/settings.py
@@ -1,9 +1,8 @@
-from .types import RegFwCmd
+from omnicomm.types import RegFwCmd
 
-
-COMMAND_PROTO: dict[RegFwCmd, str] = {
-    0x86: 'omnicomm.proto.profi2_optim2_lite2_pb2.RecReg',
+COMMAND_PROTO: dict[int | RegFwCmd, str] = {
+    0x86: 'omnicomm.proto.profi_optim_lite_pb2.RecReg',
     0x95: 'omnicomm.proto.profi2_optim2_lite2_pb2.RecReg',
-    (202000013, 114, 0x86): 'omnicomm.proto.profi2_optim2_lite2_pb2.RecReg',
-    (202000013, 0x86): 'omnicomm.proto.profi2_optim2_lite2_pb2.RecReg',
+    # (202000013, 114, 0x86): 'omnicomm.proto.profi2_optim2_lite2_pb2.RecReg',
+    # (202000013, 0x86): 'omnicomm.proto.profi2_optim2_lite2_pb2.RecReg',
 }
