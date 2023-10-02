c97a861ccc7b9219dd0527295ee62def75ebe05b
Sergei Pikhovkin <pikhovkin@gmail.com>
2023-10-02 19:40:13 +0300

    Add cast_command_proto util

--- omnicomm/utils.py
+++ omnicomm/utils.py
@@ -1,6 +1,8 @@
 import sys
 from importlib import import_module
 
+from omnicomm.types import RegFwCmd
+
 
 def cached_import(module_path, class_name):
     # Check whether module is loaded and fully initialized.
@@ -28,3 +30,33 @@ def import_string(dotted_path):
     except AttributeError as err:
         msg = f'Module "{module_path}" does not define a "{class_name}" attribute/class'
         raise ImportError(msg) from err
+
+
+def cast_command_proto(command_proto: dict[str, str]) -> dict[int | RegFwCmd, str]:
+    """Cast a command`s map from a config with `str` keys to config with `int | tuple[int, ...]` keys.
+
+    For example:
+
+    ```yaml
+    ---
+    COMMAND_PROTO:
+      (200000000, 134): omnicomm.proto.profi2_optim2_lite2_pb2.RecReg
+      '134': omnicomm.proto.profi_optim_lite_pb2.RecReg
+    ```
+
+    ```python
+    import settings
+
+    command_proto = cast_command_proto(settings.COMMAND_PROTO)
+    assert command_proto[134]
+    assert command_proto[(200000000, 134)]
+    ```
+    """
+    conf: dict[int | RegFwCmd, str] = {}
+    for k, v in command_proto.items():
+        key = tuple(map(int, k.replace(' ', '').strip('()').split(',')))
+        if len(key) == 1:
+            conf[key[0]] = v.strip()
+        else:
+            conf[key] = v.strip()
+    return conf
