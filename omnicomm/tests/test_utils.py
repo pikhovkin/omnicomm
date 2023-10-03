593d1f6994af87737b5890d5040753e0b50b5d9b
Sergei Pikhovkin <pikhovkin@gmail.com>
2023-10-03 22:39:46 +0300

    Improve utils tests

--- omnicomm/tests/test_utils.py
+++ omnicomm/tests/test_utils.py
@@ -7,11 +7,11 @@ class UtilsTest(TestCase):
     def test_cast_command_proto(self):
         command_proto = cast_command_proto(
             {
-                '123': 'path.to.proto',
-                '(1, 23)': 'path.to.proto',
-                '(1, 2, 3)': 'path.to.proto',
+                '123': 'path.to.proto1',
+                '(1, 23)': 'path.to.proto2',
+                '(1, 2, 3)': 'path.to.proto3',
             }
         )
-        self.assertTrue(command_proto[123])
-        self.assertTrue(command_proto[(1, 23)])
-        self.assertTrue(command_proto[(1, 2, 3)])
+        self.assertTrue(command_proto[123] == 'path.to.proto1')
+        self.assertTrue(command_proto[(1, 23)] == 'path.to.proto2')
+        self.assertTrue(command_proto[(1, 2, 3)] == 'path.to.proto3')
