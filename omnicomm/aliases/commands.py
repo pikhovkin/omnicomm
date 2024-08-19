9d7f2ce9a73460d5dddbd4ec2424b05936a42754
alex-eri <alex-eri@users.noreply.github.com>
2024-08-19 11:24:14 +0300

    Fixes Response typos

--- omnicomm/aliases/commands.py
+++ omnicomm/aliases/commands.py
@@ -3,12 +3,12 @@ from omnicomm.commands import *
 CmdRegistarIdentity = Cmd80
 CmdServerIdentity = Cmd81
 CmdGetRecord = Cmd85
-CmdRecordResponce = Cmd86
+CmdRecordResponse = Cmd86
 CmdDeleteRecord = Cmd87
-CmdRecordResponceAck = Cmd87
+CmdRecordResponseAck = Cmd87
 CmdDeleteRecordAck = Cmd88
 CmdGetTime = Cmd93
 CmdTimeResponse = Cmd94
 CmdRecordStream = Cmd95
-CmdRecordResponceWiFi = Cmd9F
-CmdRecordResponceWiFiAck = CmdA0
+CmdRecordResponseWiFi = Cmd9F
+CmdRecordResponseWiFiAck = CmdA0
